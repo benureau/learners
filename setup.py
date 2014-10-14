f82d963e987d5bbe2f98549bcc124ffadeb1548b
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-10-14 12:58:36 +0200

    1.0 version, switch to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = "learners",
-    version = "0.1",
+    version = "1.0",
     author = "Fabien Benureau",
     author_email = "fabien.benureau@inria.fr",
     description = ("A collection of simple incremental learning algorithm"),
-    license = "Open Science.",
+    license = "Open Science (see fabien.benureau.com/openscience.html)",
     keywords = "learning algorithm",
     url = "flowers.inria.fr",
     packages=['learners', 'learners.algorithms'],
-    classifiers=[],
 )
