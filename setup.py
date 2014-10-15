000797f56b38fc590b59edebb5431a0bdccfa4cd
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-10-15 02:02:38 +0200

    update setup.py to add forest, numpy requirements

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ setup(
     author = "Fabien Benureau",
     author_email = "fabien.benureau@inria.fr",
     url='flowers.inria.fr',
-    download_url='
+    download_url='https://github.com/humm/learners/tarball/1.0',
     maintainer='Fabien Benureau',
     description = "A collection of simple incremental forward and inverse model learning algorithms",
     license = "Open Science License (see fabien.benureau.com/openscience.html)",
     keywords = "learning algorithm",
     packages=['learners', 'learners.algorithms'],
+    install_requires=['forest','numpy'],
+    dependency_links=[
+        "https://github.com/flowersteam/forest/tarball/master#egg=forest-1.0",
+    ],
 )
