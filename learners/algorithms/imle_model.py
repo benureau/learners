86543bdd0def0c8b08c5291e3142ac7e9ae34a45
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-05-22 15:38:33 +0200

    imle package is now a dynamic import at instantiation

--- learners/algorithms/imle_model.py
+++ learners/algorithms/imle_model.py
@@ -4,11 +4,8 @@ import numbers
 import collections
 
 import numpy as np
-try:
-    import imle
-except ImportError:
-    print('To use the ImleLearner, you need to install imle first')
-    raise
+imle = None # the imle package will be imported at the first class instanciation.
+
 
 from .. import learner
 from .. import tools
@@ -16,8 +13,11 @@ from .. import tools
 
 defcfg = learner.Learner.defcfg._copy(deep=True)
 defcfg.classname = 'learners.ImleLearner'
-defcfg._describe('m_disturb', instanceof=(numbers.Real, collections.Iterable),
-                 docstring='Maximum distance of disturbance along each dimension.')
+defcfg._describe('sigma0', instanceof=(numbers.Real), default=1.0/30,
+                 docstring='sigma0 parameter for imle')
+defcfg._describe('psi0', instanceof=(numbers.Real, collections.Iterable), default=(1.0/30)**2,
+                 docstring='psi0 parameter for imle')
+
 
 class ImleLearner(learner.Learner):
     """"""
@@ -25,7 +25,22 @@ class ImleLearner(learner.Learner):
 
     def __init__(self, cfg):
         super(ImleLearner, self).__init__(cfg)
-        self.imle = imle.Imle(d=len(self.m_channels), D=len(self.s_channels))
+        self._dynamic_import()
+        self.sigma0 = self.cfg.sigma0
+        self.psi0   = self.cfg.psi0
+        if isinstance(self.psi0, numbers.Real):
+            self.psi0 = np.array([self.psi0 for _ in range(len(self.s_channels))])
+        self.imle = imle.Imle(d=len(self.m_channels), D=len(self.s_channels), sigma0=self.sigma0, psi0=self.psi0)
+
+    @classmethod
+    def _dynamic_import(cls):
+        global imle
+        if imle is None:
+            try:
+                import imle
+            except ImportError:
+                print('To use the ImleLearner, you need to install imle first')
+                raise
 
     def _predict(self, data):
         """Predict the effect of an order"""
@@ -36,10 +51,6 @@ class ImleLearner(learner.Learner):
         y = np.array(tools.to_vector(s_signal, self.s_channels))
 
         res = self.imle.predict_inverse(y, weight=True)
-        print(res)
-
-        print(res['prediction'][np.argmax(res['weight'])])
-
         return tools.to_signal(res['prediction'][np.argmax(res['weight'])],
                                self._uni_m_channels)
 
